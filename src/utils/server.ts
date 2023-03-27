87f0787d27d1d5ada8cb8b59a42216c8ffaf0c5c
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-03-27 10:39:59 -0400

    fix: trying to fix cors error

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,24 +1,22 @@
 import express from 'express';
 import routes from '../routes';
 import cors from 'cors';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 function createServer() {
   const app = express();
 
   app.use(express.json());
 
-  const allowedOrigins = ['https://gateway-list-app.vercel.app/'];
-
+  app.use(cors());
   app.use(
-    cors({
-      origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-          const msg =
-            'The CORS policy for this site does not allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
+    '/api',
+    createProxyMiddleware({
+      target: 'https://gateway-list-app.vercel.app/',
+      changeOrigin: true,
+      secure: false,
+      onProxyRes: function (proxyRes: any, req: any, res: any) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
       },
     }),
   );
