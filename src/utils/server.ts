ce5be6b4406ef2953fc44b275f43bc8f120c864e
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-03-27 10:08:33 -0400

    feat: add cors

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,10 +1,27 @@
 import express from 'express';
 import routes from '../routes';
+import cors from 'cors';
 
 function createServer() {
   const app = express();
 
   app.use(express.json());
+
+  const allowedOrigins = ['https://gateway-list-app.vercel.app/'];
+
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+          const msg =
+            'The CORS policy for this site does not allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      },
+    }),
+  );
 
   routes(app);
 
