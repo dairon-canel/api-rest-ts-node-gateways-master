08891ea078ced5df25e31aeadabe5371af4a9690
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-03-27 11:04:40 -0400

    feat: add dist

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,25 +1,12 @@
 import express from 'express';
 import routes from '../routes';
-import cors from 'cors';
-import { createProxyMiddleware } from 'http-proxy-middleware';
 
 function createServer() {
   const app = express();
 
   app.use(express.json());
 
-  app.use(cors());
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://gateway-list-app.vercel.app/',
-      changeOrigin: true,
-      secure: false,
-      onProxyRes: function (proxyRes: any, req: any, res: any) {
-        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-      },
-    }),
-  );
+  app.use(express.static('dist'));
 
   routes(app);
 
